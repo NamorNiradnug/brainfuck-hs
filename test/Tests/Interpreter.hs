0b44fe602ae3fee808eee71ded2d124ef4196ff5
NamorNiradnug <roma57linux@gmail.com>
2026-03-29 22:54:02 +0300

    Test hello world

--- test/Tests/Interpreter.hs
+++ test/Tests/Interpreter.hs
@@ -1,6 +1,20 @@
 module Tests.Interpreter where
-import Test.Tasty
 
+import qualified Brainfuck as BF
+import Brainfuck.Runtime.Pure (runPureRuntime)
+import Data.Char
+import Data.Either
+import Test.Tasty
+import Test.Tasty.HUnit
+import Tests.Examples
 
 tests :: TestTree
-tests = testGroup "Interpreter" []
+tests = testGroup "Interpreter" [runHelloWorld]
+
+runExampleWith :: Example -> String -> String -> Assertion
+runExampleWith Example {..} input expectedOutput =
+  map chr <$> runPureRuntime (BF.interpret $ fromRight undefined $ BF.parse name code) (map ord input)
+    @?= Just expectedOutput
+
+runHelloWorld :: TestTree
+runHelloWorld = testCase (name helloWorld) $ runExampleWith helloWorld "" "Hello World!\n"
